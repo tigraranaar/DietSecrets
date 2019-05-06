4b0150dd9bb300f2109ff9450d36b5f073a657ba
derasan <fibonacci>
2019-05-06 06:57:30 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+import * as $ from 'jquery';
 import './styles/main.scss';
 import "bootstrap";
+
+$( document ).ready(function() {
+    console.log('ready!');
+});
+
+$('#carousel-example222').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item11').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            if (e.direction=="left") {
+                $('.carousel-item11').eq(i).appendTo('.carousel-inner111');
+            }
+            else {
+                $('.carousel-item11').eq(0).appendTo('.carousel-inner111');
+            }
+        }
+    }
+});    
